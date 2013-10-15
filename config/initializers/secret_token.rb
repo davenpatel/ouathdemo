6b3712af68cb835cc22e7a773e155859d0bbad10
Daven Patel <davenptl@gmail.com>
2013-10-14 20:15:33 -0700

    V1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ouathdemo::Application.config.secret_key_base = 'b6de0d3d6d0946df42eef56f162479f5016ba28db51456dcbef51ba0317208b256580f143a275773b2e9c1202dbf1434c9204c751d089d45b805ed74ac6f2fea'
